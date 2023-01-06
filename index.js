4df1ee57ed392c52aff08a78b5fe37bd9bcc0eab
diego <diego.vignau.manjarrez@gmail.com>
2023-01-05 18:18:46 -0600

    User usecases CRUD created, and also CRUD for user pets created. Still needs come error management as it is a WORK IN PROGRESS

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ const app = express();
 const config = require("./src/lib/config");
 const db = require("./src/lib/db");
 
-const User = require("./src/models/user").model;
-
+const User = require("./src/usecases/user")
 
 app.use(express.json());
 
   // Ejecutando el servidor HTTP
 app.listen(config.app.port, async () => {
   console.log(`Esuchando peticiones HTTP en el puerto ${config.app.port}`);
-
+  
   try {
     await db.connect();
     console.log("DB is connected 🤠");
+    //const user_creation = await User.create({email:'papasflacas@supercalamar.com', password:'mes_trece', firstName:'Diego', lastName:'Viganu', phone:'12341234', street:'periferico', number:1234, interior: 1, neighborhood:'uno', municipality:'coyoacan', state:'cdmx', zipCode:4000})
+    //console.log(user_creation)
+    const user = await User.findByEmail("papasflacas@supercalamar.com");
+    const user_id = user._id;
+    //console.log(user)
+    //const add_pets = await User.pets.addPet({user_id, pet_id:1, name:'norris2', size:3, species:2})
+    //const pet = await User.pets.delPet({user_id, name:'norris2'});
+    const updated_user = await User.pets.updatePet({user_id, pet_id:2, name:'gustavo'})
+    console.log(updated_user)
   } catch (err) {
     console.error("Connection refused:", err);
   }
-  const user = new User({email:'diegovm', password:'asdf', firstName:'Diego', lastName:'Viganu', phone: '12', address: {street: "pdte", number:1, interior:1, neighborhood: "coyovia", municipality: "coyo", state:"cdmx", zipCode:4000}, suscription: {packages:['asdf']},});
-  const status = await user.save();
-  console.log(status, "este es el estado")
 });
---
+++ src/usecases/user/pets.js
@@ -0,0 +1,52 @@
+const { update } = require(".");
+
+const User = require("../../models/user").model;
+
+const addPet = async (data) => {
+    const {user_id, pet_id, name, size, species} = data;
+    const user = await User.findById(user_id);
+    user.pets.push({pet_id, name, size, species});
+    const new_pet_user = await User.findByIdAndUpdate(user_id, user);
+    return new_pet_user;
+}; 
+
+const getPet = async (data) => {
+    const {user_id, name} = data;
+    const user = await User.findById(user_id);
+    const pet = user.pets.find(item => item.name = name);
+    return pet;
+};
+
+const getAllPets = async (data) => {
+    const {user_id} = data;
+    const user = await User.findById(user_id);
+    const pets = user.pets;
+    return pets;
+};
+
+const updatePet = async (data) => {
+    const {user_id, pet_id, name, size, species} = data;
+    const user = await User.findById(user_id);
+    const pet_index = user.pets.findIndex(item => item.pet_id == pet_id);
+    user.pets[pet_index].name = name ? name : user.pets[pet_index].name;
+    user.pets[pet_index].species = species ? species : user.pets[pet_index].species;
+    user.pets[pet_index].size = size ? size : user.pets[pet_index].size;
+    const updated_pet_user = await User.findByIdAndUpdate(user_id, user);
+    return updated_pet_user;
+};
+
+const delPet = async (data) => {
+    const {user_id, name} = data;
+    const user = await User.findById(user_id);
+    user.pets = user.pets.filter(item => item.name != name);
+    const updated_user = await User.findByIdAndUpdate(user_id, user);
+    return updated_user;
+};
+
+module.exports = {
+    addPet,
+    getPet,
+    getAllPets,
+    updatePet,
+    delPet,
+};
