bf503f91afad87ab836dedc03e5915b66f0b62f2
yosassenav <mromerovanessa.soy@gmail.com>
2023-01-20 01:20:13 -0500

    adding getPaymentByDate usecase

--- src/usecases/payments/index.js
+++ src/usecases/payments/index.js
@@ -15,7 +15,7 @@ const createPayment = async (data) => {
   return await newPayment.save();
 };
 const updatePayment = async (id, data) => {
-  const { amount, status, date } = data;
+  const { amount, payment_status, date } = data;
   return await Payment.findByIdAndUpdate(id, {
     amount,
     payment_status,
@@ -26,6 +26,8 @@ const updatePayment = async (id, data) => {
 const getAllPayments = async () => await Payment.find({});
 const getPaymentById = async (id) => await Payment.findById(id);
 const delPayment = async (id) => await Payment.findByIdAndDelete(id).exec();
+const getPaymentByDate = async (payment_date) =>
+  await Payment.find({ date: payment_date });
 
 /** El caso de uso para traer los pagos de un usuario, debe ir colocado en los casos de uso de usuario. Porque el schema de usuario esta haciendo referencia al schema de pagos, por lo que al hacer el caso de uso podemos utilizar el id del pago como parametro para encontrar la informacion. Esto no se podria hacer al reves, ya que el schema de pago no esta haciendo referencia al schema de usuario */
 // const getPaymentByUser = async (userId) =>
@@ -36,5 +38,6 @@ module.exports = {
   updatePayment,
   getAllPayments,
   getPaymentById,
+  getPaymentByDate,
   delPayment,
 };
