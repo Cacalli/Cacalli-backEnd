f84ac6087e90260fe86622fa96e5e1ecbdf46a7c
diego <diego.vignau.manjarrez@gmail.com>
2023-01-28 23:11:57 -0600

    routes index file added for trafic control

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,11 @@
-//Route handler
+const userRouter = require("./userRouter");
+const packageRouter = require("./packageRouter");
+const paymentRouter = require("./paymentRouter");
+
+const apiRouter = (app) => {
+  app.use("/user", userRouter);
+  app.use("/package", packageRouter);
+  app.use("/payment", paymentRouter);
+};
+
+module.exports = apiRouter;
