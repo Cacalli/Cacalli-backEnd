859049e5fa18a263f6492fe1ff68f04038a5566e
yosassenav <mromerovanessa.soy@gmail.com>
2023-02-15 01:01:45 -0500

    adding more endpoints to customerRoutes.js file: retrieve, update, list all and delete

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -23,4 +23,59 @@ routes.post("/", async (req, res) => {
   }
 });
 
+//retrieve customer
+routes.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const retrievedCustomer = await stripe.customers.retrieve(id);
+    res.status(200).json({ ok: true, payload: retrievedCustomer });
+  } catch (error) {
+    const { message } = error;
+    res.status(400).json({ ok: false, message });
+  }
+});
+
+//update customer
+routes.post("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone, address } = req.body;
+  try {
+    const updatedCustomer = await stripe.customers.update(id, {
+      name,
+      email,
+      phone,
+      address,
+    });
+    res.status(200).json({ ok: true, payload: updatedCustomer });
+  } catch (error) {
+    const { message } = error;
+    res.status(400).json({ ok: false, message });
+  }
+});
+
+//list all customers
+routes.get("/", async (req, res) => {
+  try {
+    const customers = await stripe.customers.list({
+      limit: 3,
+    });
+    res.status(200).json({ ok: true, payload: customers });
+  } catch (error) {
+    const { message } = error;
+    res.status(400).json({ ok: false, message });
+  }
+});
+
+//delete customer
+routes.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await stripe.customers.del(id);
+    res.status(200).json({ ok: true, payload: deleted });
+  } catch (error) {
+    const { message } = error;
+    res.status(400).json({ ok: false, message });
+  }
+});
+
 module.exports = routes;
