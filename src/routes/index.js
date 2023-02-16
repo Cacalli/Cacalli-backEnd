3a8b1256a5288b0871622af4f522198e407435b7
yosassenav <mromerovanessa.soy@gmail.com>
2023-02-16 01:34:08 -0500

    adding paymentMethods to the routes catalog

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 //Route handler
 const chargesRoutes = require("./chargesRoutes");
 const paymentIntentRoutes = require("./paymentIntentsRoutes");
+const paymentMethodRoutes = require("./paymentMethodRoutes");
 const customerRoutes = require("./customerRoutes");
 const subscriptionRoutes = require("./subscriptionRoutes");
 
 const apiRouter = (app) => {
   app.use("/charges", chargesRoutes);
   app.use("/paymentIntents", paymentIntentRoutes);
+  app.use("/paymentMethod", paymentMethodRoutes);
   app.use("/customers", customerRoutes);
   app.use("/subscriptions", subscriptionRoutes);
 };
