3dabd113b98f44ec8711fe6dfc5748160b218374
yosassenav <mromerovanessa.soy@gmail.com>
2023-02-19 17:29:47 -0500

    making corrections to routes scafolding and deleting payment.js file from models folder

---
+++ src/routes/cacalliRoutes/zoneRouter.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const { checkZipCode } = require("../../usecases/zone");
+
+const routes = Router();
+
+routes.get("/checkZipCode", async (req, res) => {
+  const { zipCode } = req.body;
+
+  try {
+    const payload = await checkZipCode({ zipCode });
+    res.json({ ok: true, payload });
+  } catch (error) {
+    const { message } = error;
+    res.status(400).json({ ok: false, message });
+  }
+});
+
+module.exports = routes;
