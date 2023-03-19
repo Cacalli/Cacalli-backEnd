7d117b45fa82741a562d86262ea64908856bff9e
diego <diego.vignau.manjarrez@gmail.com>
2023-03-19 12:04:23 -0600

    first pickup created

--- src/routes/cacalliRoutes/adminRouter.js
+++ src/routes/cacalliRoutes/adminRouter.js
@@ -4,6 +4,33 @@ const { authHandler, authAdminHandler } = require ("../../middlewares/authHandle
 
 const routes = Router();
 
+/**
+ * @swagger
+ * /admin/users:
+ *   get:
+ *     summary: Checks if the specified zipcode has service available
+ *     tags: 
+ *       - admin
+ *     description: 
+ *     parameters:
+ *         - in: query
+ *           name: zone
+ *           description: 
+ *           schema:
+ *             type: string
+ *     responses:
+ *       200:
+ *         description: A boolean variable indicating if there are any zones that include that zipcode.          
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 payload:
+ *                   type: boolean
+ *                   description: The availability of a zone that includes de zipcode
+ *                   example: false
+ */
 routes.get("/users", authHandler, authAdminHandler, async (req, res) => {
     const { zone, day, time, status } = req.query;
     try {
--- src/usecases/user/pickups.js
+++ src/usecases/user/pickups.js
@@ -42,16 +42,14 @@ const delayPickup = async (id) => {
 };
 
 const getNextWeekDay = (date, weekDay) => {
-    console.log('asdf', dage, weekDay);
     const newDate = date;
-    console.log(date.getDate(), weekDay);
     newDate.setDate(date.getDate() + ((7 - date.getDay() + weekDay) % 7 || 7));
     return newDate;
 };
 
 const getPickupPeriod = async (id) => {
     const user = await User.findById(id);
-    const packageId = user.subscription.packages[0];
+    const packageId = user.subscription.packages[0].packageId;
     const mainPackage = await package.getById(packageId);
     const pickupPeriod = mainPackage.pickupPeriod;
     return pickupPeriod;
@@ -60,13 +58,10 @@ const getPickupPeriod = async (id) => {
 const createFirstPickup = async (id) => {
     const user = await User.findById(id);
     const now = new Date();
-    console.log('antes');
-    console.log(user.pickupInfo.day);
     let newDate = getNextWeekDay(now, user.pickupInfo.day);
-    console.log(newDate);
     const pickupPeriod = await getPickupPeriod(id);
     if(pickupPeriod == 2){
-        newDate = newDate + 7;
+        newDate.setDate(newDate.getDate() + 7);
     }
     const newPickup = await pickup.create({user: id, date: newDate});
     return newPickup;
